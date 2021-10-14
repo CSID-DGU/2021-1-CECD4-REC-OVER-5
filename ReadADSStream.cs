7da5f4526b4849829abde59756d777390d051d22
choyeongwook <72301213+choyeongwook@users.noreply.github.com>
2021-10-14 17:25:23 +0900

    Update ReadADSStream.cs

--- ReadADSStream.cs
+++ ReadADSStream.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using Trinet.Core.IO.Ntfs;
 using System.Text;
@@ -9,18 +9,19 @@ class ReadADSStream {
     {
         //********** 파일 접근 **********
 
-        FileInfo file = new FileInfo("c:\\test\\4주차.pdf");
+        FileInfo file = new FileInfo("c:\\test\\test.txt");
         //*******************************
 
 
 
         //********** 파일에 ADS스트림 생성 및 쓰기 **********
 
-        //생성할 ADS스트림 이름
-        AlternateDataStreamInfo makeStream = file.GetAlternateDataStream("testgogo", FileMode.Create);
+        //생성할 ADS스트림을 지우고 새로 만듦
+        file.DeleteAlternateDataStream("RudyFlag");
+        AlternateDataStreamInfo makeStream = file.GetAlternateDataStream("RudyFlag", FileMode.Create);
 
         //ADS 스트림 안에 넣을 문자열
-        Byte[] information = new UTF8Encoding(true).GetBytes("드디어된다시발");
+        Byte[] information = new UTF8Encoding(true).GetBytes("scanned2021-10-13");
 
 
         using (FileStream fs = makeStream.OpenWrite())
@@ -57,7 +58,7 @@ class ReadADSStream {
         //Zone.Identifier 스트림을 읽어 존재하면 다운로드 파일, 존재하지 않으면 비다운로드 파일
         if (file.AlternateDataStreamExists("Zone.Identifier"))
         {
-            Console.WriteLine("다운로드 파일입니다. :");
+            Console.WriteLine("다운로드 파일입니다. \nDOWNLOAD URL: ");
 
             AlternateDataStreamInfo s = file.GetAlternateDataStream("Zone.Identifier", FileMode.Open);
             using (TextReader reader = s.OpenText())
@@ -77,6 +78,6 @@ class ReadADSStream {
 
 
         //스트림 삭제
-        //file.DeleteAlternateDataStream("info");
+        //file.DeleteAlternateDataStream("testgogo");
     }
 }
