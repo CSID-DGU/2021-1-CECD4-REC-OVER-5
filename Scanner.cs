4ee490de32024d1e6db63cdfc4736a6a03418153
choyeongwook <72301213+choyeongwook@users.noreply.github.com>
2021-10-30 20:40:42 +0900

    Update Scanner.cs

--- Scanner.cs
+++ Scanner.cs
@@ -17,7 +17,7 @@ namespace RUDY_SCANNER
             List<string> fileCreationTimeList = new List<string>();
             List<string> fileLastWriteTimeList = new List<string>();
             List<string> fileLastAccessTimeList = new List<string>();
-            List<bool> fileDownloadList = new List<bool>();
+            List<bool> fileIsDownloadedList = new List<bool>();
             List<string> fileFlagList = new List<string>();
 
             FileInfo info;
@@ -31,9 +31,9 @@ namespace RUDY_SCANNER
                 fileLastAccessTimeList.Add(info.LastAccessTime.ToString("yyyy/MM/dd hh:mm:ss"));
 
                 if (info.AlternateDataStreamExists("Zone.Identifier"))
-                    fileDownloadList.Add(true);
+                    fileIsDownloadedList.Add(true);
                 else
-                    fileDownloadList.Add(false);
+                    fileIsDownloadedList.Add(false);
 
                 if (!info.AlternateDataStreamExists(streamName))
                     fileFlagList.Add(" ");
@@ -47,7 +47,7 @@ namespace RUDY_SCANNER
                 }
             }//end of insert metadata loop
 
-            foreach (string str in fileNameList)  //리스트이름 바꿔서 다른정보 출력해 볼 수 있음
+            foreach (string str in fileNameList)  //리스트이름 바꿔서 다른정보 출력해 볼 수 있음(다운로드유무만 스트링말고 bool임)
             {
                 Console.WriteLine(str);
             }
